67ce2758beee40569f398c6722b8fbbe0a440647
DrogrinHunter <lukewillmore67@gmail.com>
2022-08-26 16:35:59 +0100

    user booking.html added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// This is for the side bars 
 function opentr(n, arg) {
   if (typeof active !== 'undefined') {
     console.log("Clearing interval");
@@ -36,4 +37,40 @@ function expandmenu(id) {
 }
 
 setTimeout(menu, 500, "dashboard");
+
+// Doughnuts on user home page
+
+const ctx = document.getElementById('userHoliday').getContext('2d');
+const data = {
+  labels: [
+    'Used',
+    'To Take'
+  ],
+  datasets: [{
+    label: 'Holiday to take',
+    data: [28, 5], // first figure is used, second is to take 
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+    ],
+    hoverOffset: 4
+  }]
+};
+
+const userHoliday = new Chart(ctx, {
+  type: 'doughnut',
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'left',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Doughnut Chart'
+      }
+    }
+  },
+});
 
