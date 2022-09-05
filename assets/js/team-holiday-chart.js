4aa46d4b8f19362b9a46dedf418d8b5ca8a82b4b
DrogrinHunter <lukewillmore67@gmail.com>
2022-09-05 16:55:32 +0100

    added additional chart

--- assets/js/team-holiday-chart.js
+++ assets/js/team-holiday-chart.js
@@ -1,36 +1,45 @@
+const Utils = chartUtils.init();
 
 // team check holiday section
 
 const thctx = document.getElementById('teamHoliday').getContext('2d')
+const DATA_COUNT = 7;
+const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
+
+const labels = Utils.months({count: 7});
 const dataTeamHoliday = {
-  labels: [
-    'Used',
-    'To Take'
-  ],
-  datasets: [{
-    label: 'Holiday to take',
-    data: [28, 5], // first figure is used, second is to take 
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-    ],
-    hoverOffset: 4
-  }]
+  labels: labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => {
+        return [Utils.rand(-100, 100), Utils.rand(-100, 100)];
+      }),
+      backgroundColor: Utils.CHART_COLORS.red,
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => {
+        return [Utils.rand(-100, 100), Utils.rand(-100, 100)];
+      }),
+      backgroundColor: Utils.CHART_COLORS.blue,
+    },
+  ]
 };
 
 const teamHoliday = new Chart(thctx, {
-  type: 'bar',
-  data: dataTeamHoliday,
-  options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Team holiday'
-      }
+    type: 'bar',
+    data: dataTeamHoliday,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Floating Bar Chart'
+            }
+        }
     }
-  },
 });
