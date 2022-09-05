4ba793021cbc31698741f4ef69a143f509d4159f
DrogrinHunter <lukewillmore67@gmail.com>
2022-09-05 15:26:52 +0100

    separated js into different files

---
+++ archive/menuBar.js
@@ -0,0 +1,40 @@
+
+// This is for the side bars 
+function opentr(n, arg) {
+    if (typeof active !== 'undefined') {
+      console.log("Clearing interval");
+      active = false;
+    }
+  
+    $("#main-content").fadeOut("fast", function () {
+      $("#main-content").html('');
+      $.get(n + ".php", { zone_uuid: arg })
+        .done(function (data) {
+          $("#main-content").html(data);
+          $("#main-content").fadeIn("fast");
+        });
+    });
+  }
+  
+  function menu(n) {
+    if (typeof active !== 'undefined') {
+      console.log("Clearing interval");
+      active = false;
+    }
+  
+    $("#main-content").fadeOut("fast", function () {
+      $("#main-content").html('');
+      $.get(n + ".php", function (data) {
+        $("#main-content").html(data);
+        $("#main-content").fadeIn("fast");
+      });
+    });
+  }
+  
+  // expand menu
+  function expandmenu(id) {
+    $(".sub-items").slideUp("fast");
+    $("#" + id).slideDown("fast");
+  }
+  
+  setTimeout(menu, 500, "dashboard");
